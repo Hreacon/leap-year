7bbf12ba101c5c76dff79fb6a5ac8327651ba462
Chris Kuiper and Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-19 08:26:42 -0800

    add feature next/prev leap year

--- leap-year/js/scripts.js
+++ leap-year/js/scripts.js
@@ -8,9 +8,20 @@ $(document).ready(function() {
     var result = leapYear(year);
 
     $(".year").text(year);
+    $("#next").hide();
 
     if (!result) {                 // same as writing if (result === false)
       $(".not").text("not");
+      for(var nextLeapYear = year; nextLeapYear <= year+4; nextLeapYear++) {
+        if(leapYear(nextLeapYear))
+          $(".nextLeapYear").text(nextLeapYear);
+        $("#next").show();
+      }
+      for(var prevLeapYear = year; prevLeapYear >= year-4; prevLeapYear--) {
+        if(leapYear(prevLeapYear))
+          $(".prevLeapYear").text(prevLeapYear);
+        $("#prev").show();
+      }
     } else {
       $(".not").text("");
     }
